0ac53a3f8858177e81e2a8c27c16a414b04d839c
f25ba046 <f25ba046@chuo.sc.jp>
2026-02-24 14:58:19 +0900

    Tickシステムの基礎を(TickableとGameLoop)実装

---
+++ Assets/Scripts/Application/ITickable.cs
@@ -0,0 +1,11 @@
+namespace Application_進行
+{
+    /// <summary>
+    /// 更新処理が必要なクラスに実装するインターフェース
+    /// </summary>
+    public interface ITickable
+    {
+        // deltaTimeは前のフレームからの経過時間
+        void Tick(float deltaTime);
+    }
+}
