c760e6faaddf164232d8bbc36263ff99aff80ea1
PotatoTimeKun <ichikota8440@gmail.com>
2026-02-26 16:31:03 +0900

    ネームスペースを削除

--- Assets/Scripts/Application/ITickable.cs
+++ Assets/Scripts/Application/ITickable.cs
@@ -1,11 +1,8 @@
-namespace Application_進行
+/// <summary>
+/// 更新処理が必要なクラスに実装するインターフェース
+/// </summary>
+public interface ITickable
 {
-    /// <summary>
-    /// 更新処理が必要なクラスに実装するインターフェース
-    /// </summary>
-    public interface ITickable
-    {
-        // deltaTimeは前のフレームからの経過時間
-        void Tick(float deltaTime);
-    }
+    // deltaTimeは前のフレームからの経過時間
+    void Tick(float deltaTime);
 }
