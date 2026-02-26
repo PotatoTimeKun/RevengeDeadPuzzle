577b4c319bc781ccfc3d27d5037e30996c595388
PotatoTimeKun <ichikota8440@gmail.com>
2026-02-26 16:28:33 +0900

    PlayerLogicのベースを作成

--- Assets/Scripts/Domain/PlayerLogic.cs
+++ Assets/Scripts/Domain/PlayerLogic.cs
@@ -3,5 +3,10 @@
 public class PlayerLogic : ITickable
 {
     public int HP;
-    
+    public Entity_Data.PlayerState State;
+    public Entity_Data.DeathType Type;
+    public Tick(float deltaTime){}
+    public Die(Entity_Data.DeathType deathType){
+        
+    }
 }
