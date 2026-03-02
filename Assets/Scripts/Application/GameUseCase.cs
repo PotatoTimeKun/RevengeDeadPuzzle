7b6d297f2f307630826267643a1a6dfd695ae6e5
Cocoa <syu.2830stl@gmail.com>
2026-03-02 16:28:22 +0900

    修正

--- Assets/Scripts/Application/GameUseCase.cs
+++ Assets/Scripts/Application/GameUseCase.cs
@@ -4,8 +4,29 @@ public class GameUseCase : MonoBehaviour , ITickable
 {
     private PlayerController _playerController;
     private StageDef _stageDef;
-    public void StartGame(){}
-    public void OnPlayerDead(Entity_Data.DeathType deathType){}
+    [SerializeField] private GameObject _startPos;
+    public GameObject DEBUGPlayerPrefab; // 仮置き、実際にはCostumeRedistoryから持ってこれるようにする
+    void Start(){
+        StartGame();
+        GameLoop.Instance.Register(this);
+    }
+    void OnDestroy(){
+        GameLoop.Instance.Unregister(this);
+    }
+    public void StartGame(){
+        GameObject PlayerObj = Instantiate(DEBUGPlayerPrefab);
+        _playerController = PlayerObj.GetComponent<PlayerController>();
+        PlayerObj.transform.position = _startPos.transform.position;
+    }
+    public void OnPlayerDead(Entity_Data.DeathType deathType){
+        GameObject PlayerObj = Instantiate(DEBUGPlayerPrefab);
+        _playerController = PlayerObj.GetComponent<PlayerController>();
+        PlayerObj.transform.position = _startPos.transform.position;
+    }
     public void OnGoal(){}
-    public void Tick(float deltaTIme){}
+    public void Tick(float deltaTime){
+        // 死んだときに処理を実行
+        if (_playerController.PlayerLogic.State != Entity_Data.PlayerState.Dead) return;
+        OnPlayerDead(_playerController.PlayerLogic.Type);
+    }
 }
