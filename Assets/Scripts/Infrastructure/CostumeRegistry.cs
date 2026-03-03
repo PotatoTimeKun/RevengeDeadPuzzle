124666b28b6e68459baf4e9eae11a3a1b3e98e45
PotatoTimeKun <ichikota8440@gmail.com>
2026-03-03 14:45:31 +0900

    エディタ上で表示するとエラーになるバグを修正

--- Assets/Scripts/Infrastructure/CostumeRegistry.cs
+++ Assets/Scripts/Infrastructure/CostumeRegistry.cs
@@ -1,17 +1,36 @@
-using NUnit.Framework;
 using UnityEngine;
 using System.Collections.Generic;
+
+#if UNITY_EDITOR
+
+// インスペクター表示にエラーが出るので旧方式に変更
+
+using UnityEditor;
+[CustomEditor(typeof(CostumeRegistry))]
+public class CostumeRegistryEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        // UI Toolkit (新方式) を使わず、IMGUI (旧方式) で描画する
+        serializedObject.Update();
+        
+        // 従来の「標準的な見た目」でリストを描画
+        DrawDefaultInspector();
+        
+        serializedObject.ApplyModifiedProperties();
+    }
+}
+#endif
 
 [CreateAssetMenu(fileName = "CostumeRegistry", menuName = "Scriptable Objects/CostumeRegistry")]
 public class CostumeRegistry : ScriptableObject
 {
-    public List<CostumeDef> AllCostume;
+    public List<CostumeDef> AllCostume = new();
 
-    private Dictionary<string, CostumeDef> AllCostumeDic;
+    [System.NonSerialized]
+    private Dictionary<string, CostumeDef> AllCostumeDic = new();
 
-
-    private void OnEnable()
-    {
+    void OnEnable(){
         Initialize();
     }
 
