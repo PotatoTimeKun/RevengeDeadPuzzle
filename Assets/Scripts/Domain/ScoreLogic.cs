a649fb251b6822c1eaa5e6a67065d21340e01996
Cocoa <syu.2830stl@gmail.com>
2026-03-05 16:32:21 +0900

    ScoreLogicを作成

--- Assets/Scripts/Domain/ScoreLogic.cs
+++ Assets/Scripts/Domain/ScoreLogic.cs
@@ -1,11 +1,40 @@
 // UnityEngine禁止
 using System.Collections.Generic;
 
-public class ScoreLogic
+public class ScoreLogic : ITickable
 {
     private float _currentTime;
-    public float CurrentTime{get {return _currentTime;}}
+    private StageDef _currentStage;
+    public float CurrentTime { get { return _currentTime; } }
     public int DeathCount;
-    public List<Entity_Data.DeathType> DeathTypeHistory = new List<Entity_Data.DeathType>();
-    public List<bool> CheckEvaluation(){return new List<bool>();}
+    public List<Entity_Data.DeathType> DeathTypeHistory = new();
+
+    public ScoreLogic(StageDef stage)
+    {
+        _currentStage = stage;
+        _currentTime = 0f;
+        DeathCount = 0;
+    }
+
+    public void Tick(float deltaTime)
+    {
+        _currentTime += deltaTime;
+    }
+
+    public void AddDeathCount(Entity_Data.DeathType deathType = Entity_Data.DeathType.None)
+    {
+        DeathCount++;
+        DeathTypeHistory.Add(deathType);
+    }
+
+    public List<bool> CheckEvaluation()
+    {
+        List<bool> evaluations = new();
+
+        evaluations.Add(CurrentTime <= _currentStage.TimerSecondTarget);
+        evaluations.Add(DeathCount <= _currentStage.DeathCountTarget);
+        evaluations.Add(DeathTypeHistory.TrueForAll(deathType => _currentStage.AcceptedDeathTypeTarget.Contains(deathType)));
+
+        return evaluations;
+    }
 }
