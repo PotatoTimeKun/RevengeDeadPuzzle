21eecb2b900d0a0916533ec58e03ba1be73651ce
f25ba046 <f25ba046@chuo.sc.jp>
2026-03-05 15:32:29 +0900

    MentaLogicを作成

--- Assets/Scripts/Domain/MentalLogic.cs
+++ Assets/Scripts/Domain/MentalLogic.cs
@@ -3,9 +3,25 @@
 public class MentalLogic
 {
     private float _currentValue;
+    private readonly float _maxValue;
     public float CurrentValue {
-        get {return _currentValue;}
+        get { return _currentValue; }
     }
-    public void Decrease(float amount){}
-    public void Recover(){}
+
+    public MentalLogic(float maxAmount)
+    {
+        _maxValue = maxAmount;
+        _currentValue = maxAmount;
+    }
+
+    public void Decrease(float amount)
+    {
+        _currentValue -= amount;
+        if (_currentValue < 0) _currentValue = 0;
+    }
+
+    public void Recover()
+    {
+        _currentValue = _maxValue;
+    }
 }
